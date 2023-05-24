d78f40d1d5c70ff350bac85697c89a9663e1d1dd
Olumide Okedusi <olumideokedusi@gmail.com>
2023-05-25 00:09:40 +0100

    Implement copy-to-clipboard functionality

--- app/gift-registry/page.tsx
+++ app/gift-registry/page.tsx
@@ -1,11 +1,25 @@
 'use client';
 
+import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import copy from '@/public/images/copy.png';
-import Image from 'next/image';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function GiftRegistry() {
+  function copyToClipboard(numString: string) {
+    navigator.clipboard.writeText(numString);
+    toast('Copied!', {
+      position: 'top-right',
+      autoClose: 500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+    });
+  }
+
   return (
     <div>
       <Header backgroundImg='bg-gift'>
@@ -20,19 +34,26 @@ export default function GiftRegistry() {
         <div className='flex flex-col justify-around h-96 w-full rounded-lg bg-[#052862] py-3 px-7 my-5 mx-5 text-center font-sans sm:w-[470px] text-white'>
           <h6 className='font-serif font-semibold text-xl'>Naira Account</h6>
           <div className='flex flex-col items-center'>
-            <h5 className=''>Opay</h5>
-            <button className='rounded-lg bg-slate-50 text-[#001944] px-5 py-1 my-2 w-[165px] font-serif font-bold flex justify-between items-center text-xl'>
-              8118117634
+            <h5 className=''>Access Bank</h5>
+            <button
+              onClick={() => copyToClipboard('0691510512')}
+              className='rounded-lg bg-slate-50 text-[#001944] px-5 py-1 my-2 w-[165px] font-serif font-bold flex justify-between items-center text-xl hover:bg-slate-200'
+            >
+              0691510512
               <Image src={copy} alt='copy' width={15} height={15} />
             </button>
+            <ToastContainer style={{ width: '200px' }} />
             <p className='font-light'>Adefiyinfoluwa Adeniyi</p>
           </div>
           <hr className='my-4 sm:my-2' />
-          <h6 className='font-serif font-semibold text-xl'>Foreign Account</h6>
+          <h6 className='font-serif font-semibold text-xl'>Naira Account</h6>
           <div className='flex flex-col items-center'>
-            <h5 className=''>First Bank Nigeria</h5>
-            <button className='rounded-lg bg-slate-50 text-[#001944] px-5 py-1 my-2 w-[150px] font-serif font-bold flex justify-around items-center text-xl'>
-              Pending
+            <h5 className=''>GT Bank </h5>
+            <button
+              onClick={() => copyToClipboard('0127642825')}
+              className='rounded-lg bg-slate-50 text-[#001944] px-5 py-1 my-2 w-[165px] font-serif font-bold flex justify-between items-center text-xl hover:bg-slate-200'
+            >
+              0127642825
               <Image src={copy} alt='copy' width={15} height={15} />
             </button>
             <p className='font-light'>Oluwatobiloba Adeyemi</p>
